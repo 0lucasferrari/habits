a58219f0ec9f6393d04ffd29ffc3974c85c0b632
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2023-01-17 23:19:32 -0300

    Install fastify cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,21 @@
 import Fastify from "fastify";
+import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-    return 'Hello World!'
+app.get('/', async () => {
+
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                'startsWith': 'Tomar'
+            }
+        }
+    })
+
+    return habits;
+
 })
 
 app.listen({
