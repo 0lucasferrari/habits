57849a2793778d114ed866cfabd645f9689cb73f
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2023-01-17 22:50:43 -0300

    Updates node version and installs Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log("Oi! E agora tchau!")
+import Fastify from "fastify";
+
+const app = Fastify();
+
+app.get('/', () => {
+    return 'Hello World!'
+})
+
+app.listen({
+    port:3333
+})
